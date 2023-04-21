cd7b946305f5d36ae44facaad6a3a7c546d21b05
Maria <maria.brunned@gmail.com>
2023-04-21 09:33:18 +0200

    load all images and show image in window

--- Lab 3/Lab3.1/main.py
+++ Lab 3/Lab3.1/main.py
@@ -1,12 +1,28 @@
 import numpy as np 
 import pandas as pd
 import cv2
+import glob
 
-img = cv2.read("01.jpg")
-print(img)
+img_rgb = cv2.imread("Lab3.1/02.jpg")
+images = [cv2.imread(file) for file in glob.glob("Lab3.1/*.jpg")] # Load in all files
 
-b,g,r = img[1,1] # Testing to see if we get the color from px in position (1,1)
+#print(img)
 
-print("b: ", b)
+b,g,r = img_rgb[1,1] # Testing to see if we get the color from px in position (1,1)
+
+# Debug
+'''print("b: ", b)
 print("g: ", g)
-print("r: ", r)
+print("r: ", r)'''
+
+# rgb -> lab 
+img_lab = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2Lab)
+
+# Debug
+print(img_lab[1,1])
+
+# Feature vectors 
+vec_color =  [[] for i in range(12)] # color
+cv2.imshow('color image', img_rgb) # Show image
+cv2.waitKey(0)
+cv2.destroyAllWindows()
