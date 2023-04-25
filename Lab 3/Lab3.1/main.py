0cd97d3a7b695744a23c21856b3dd618e80a578b
Maria <maria.brunned@gmail.com>
2023-04-25 14:56:27 +0200

    weight to the ranking
    Given weight to the different rankings and cleaned up the code

--- Lab 3/Lab3.1/main.py
+++ Lab 3/Lab3.1/main.py
@@ -10,46 +10,6 @@ img_chosen = 10 # Image 11
 images = [cv2.imread(file) for file in glob.glob("Lab3.1/*.jpg")] # Load in all files
 #img_rgb = images[img_chosen]
 
-# resize images, preserve aspect ratio
-
-# find smallest breadth and width BYT TILL MINSTA GEMENSAMMA NÄMNARE KOLLA PÅ HUR FAN MAN SKA GÖRA DET
-'''width = images_original[0].shape[1]
-height = images_original[0].shape[0]
-for i in range(1,12):
-    if width > images_original[i].shape[1]:
-        width = images_original[i].shape[1]
-
-    if height > images_original[i].shape[0]:
-        height = images_original[i].shape[0]
-
-# Aspect ratio
-width_ratio = width/100
-height_ratio = height/100
-
-# resize images
-images = [[] for i in range(12)]
-
-for i in range(12):
-    dim = (int(images_original[i].shape[1]*width_ratio), int(images_original[i].shape[0]*height_ratio))
-    resized = cv2.resize(images_original[i], dim, interpolation=cv2.INTER_AREA)
-    images[i] = resized
-    print('resized image ', i+1)
-    print('sizes of image ', i, ': ', resized.shape)'''
-
-#b,g,r = img_rgb[1,1] # Testing to see if we get the color from px in position (1,1)
-
-
-# Debug
-'''print("b: ", b)
-print("g: ", g)
-print("r: ", r)'''
-
-# rgb -> lab 
-#img_lab = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2Lab)
-
-# Debug
-#print(img_lab[1,1])
-
 # Histogram of one image
 #hist = cv2.calcHist([img_rgb], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
 #hist = hist.flatten() # Making the histogram into one vector -> feature vector
@@ -89,7 +49,6 @@ for i in range(12):
     edges_hist = cv2.calcHist(edges[i], [0], None, [8], [0, 256])
     vec_edges[i] = edges_hist.flatten()
     cv2.imshow
-    
 
     #print(i, ": ", vec_color[i][11])
 
@@ -99,10 +58,6 @@ dist_color = []
 dist_mean = []
 dist_edges = []
 dist_lum = []
-
-print(len(vec_edges[0]))
-
-
 
 for i in range(12):
     d1 = d2 = d3 = d4 = 0
@@ -122,17 +77,11 @@ for i in range(12):
         d3 += abs(vec_lum[i][j]-vec_lum[img_chosen][j])
     dist_lum.append(d3)
 
-    '''max_len = len(vec_edges[img_chosen])
-    if(len(vec_edges[i]) < max_len):
-        max_len = len(vec_edges[i])'''
-
     for j in range(len(vec_edges[i])): 
         d4 += abs(vec_edges[i][j]-vec_edges[img_chosen][j])
     dist_edges.append(d4)
 
-
-
-    print("image ", i, " distance: ", d4)
+    #print("image ", i, " distance: ", d4)
 
 
 # Gives the indexes based on the distances dist_color
@@ -141,67 +90,53 @@ ind_mean = sorted(range(len(dist_mean)), key = lambda k: dist_mean[k])
 ind_lum = sorted(range(len(dist_lum)), key = lambda k: dist_lum[k])
 ind_edges = sorted(range(len(dist_edges)), key = lambda k: dist_edges[k])
 
+# Give weights to the different rankings
+rank = np.zeros(12)
+for i in range(12):
+    rank[ind_color[i]] += i*0.2
+    rank[ind_mean[i]] += i*0.2
+    rank[ind_lum[i]] += i*0.3
+    rank[ind_edges[i]] += i*0.3
+
+# final ranking
+ind_rank = sorted(range(len(rank)), key = lambda k:rank[k])
+
 print("color ranking: ", ind_color)
 print("mean ranking: ", ind_mean)
 print("illumination ranking: ", ind_lum)
 print("edge ranking: ", ind_edges)
+print("final ranking: ", ind_rank)
 
 
 # Show one image
 '''cv2.imshow('color image', img_rgb) # Show chosen image
 cv2.waitKey(0)
 cv2.destroyAllWindows()'''
-fig = plt.figure(figsize=(10,10))
 
+'''fig = plt.figure(figsize=(10,10))
+
+# Edge images
 # Create subplots to print all images
 for i in range(12):
     fig.add_subplot(4, 3, 1+i)
     # adds the images based on the ranking from the vectors
     # Change the from BGR to RGB since cv2 loads images in BGR for some reason
-    plt.imshow(edges[ind_edges[i]])
+    plt.imshow(edges[ind_rank[i]])
     plt.axis('off')
-    plt.title(i)
+    plt.title(i)'''
 
 # Create figure
 fig = plt.figure(figsize=(10,10))
 
+# Ranking of all the images
 # Create subplots to print all images
 for i in range(12):
     fig.add_subplot(4, 3, 1+i)
     # adds the images based on the ranking from the vectors
     # Change the from BGR to RGB since cv2 loads images in BGR for some reason
-    plt.imshow(cv2.cvtColor(images[ind_edges[i]], cv2.COLOR_BGR2RGB))
+    plt.imshow(cv2.cvtColor(images[ind_rank[i]], cv2.COLOR_BGR2RGB))
     plt.axis('off')
     plt.title(i)
 
 plt.show() # Show rank imgaes
 
-''' Color Mean feature vectors'''
-'''vec_meanColor = [[]for i in range(12)]
-
-for i in range(12):
-    vec_meanColor[i] = cv2.mean(images[i])
-    vec_meanColor[i] = vec_meanColor[i][:3] # ignore last value in array, contains b, g, r!'''
-
-#print(vec_meanColor)
-
-''' Edge feature vectors'''
-'''vec_edges = [[]for i in range(12)]
-
-for i in range(12):
-    vec_edges[i] = cv2.Canny(images[i], 100, 200).flatten() # 100 and 200 are the minimum and maximum values in hysteresis thresholding.
-
-print(vec_edges)'''
-
-
-'''Luminance feature vectors'''
-'''vec_lum = [[]for i in range(12)]
-
-for i in range(12):
-    img = cv2.cvtColor(images[i], cv2.COLOR_BGR2HSV)
-
-    # Calculate the histogram for each image in the dataset
-    vec_lum[i] = cv2.calcHist([img], [2], None, [8], [0, 256])
-    vec_lum[i] = hist.flatten() # Making the histogram into one vector -> feature vector
-
-print(vec_lum)'''
