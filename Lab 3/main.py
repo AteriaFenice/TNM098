0b540d8eb88b5e0d64b3381fa31ed5eb20daa299
Maria <maria.brunned@gmail.com>
2023-04-26 11:16:26 +0200

    reshape files

--- Lab 3/main.py
+++ Lab 3/main.py
@@ -2,8 +2,17 @@ import numpy as np
 import math
 import pandas as pd
 import nltk
+from nltk import tokenize
 import os # for reading in files
 import string
+
+# remove punctuation
+def remove_punctuation(text):
+    return [sentence.translate(str.maketrans('','', string.punctuation)) for sentence in text]
+
+# make everything into lower case
+def lower_case(text):
+    return [sentence.lower() for sentence in text]
 
 # Print all files 
 path = 'Lab3.2/'
@@ -16,15 +25,33 @@ for fle in all_files:
     with open(os.path.join(path, fle)) as f:
         files[counter] = f.read()
         counter += 1
-        print(counter)
         f.close()
 
+# Remove \n
+files = [file.replace('\n', " ") for file in files]
+
 # Debug
-print(files[2])
+#print(files[2])
+print('load in all files')
+
+# Divide texts into sentences
+#nltk.download('punkt')
+#token = nltk.data.load('tokenizer/punkt/english.pickle')
+files = [tokenize.sent_tokenize(file) for file in files]
+
+print('split text into sentences')
+
 
 # Reshape text
-for i in range(0, len(files)):
-    files[i] = files[i].translate(str.maketrans('','', string.punctuation)) # Remove punctuation
-    files[i] = files[i].lower() # Lower case letters
+'''for i in range(0, len(files)):
+    files[i] = [files[i].translate(str.maketrans('','', string.punctuation))] # Remove punctuation
+    files[i] = files[i].lower() # Lower case letters'''
 
-print(files[2])
+files = [remove_punctuation(file) for file in files]
+files = [lower_case(file) for file in files]
+
+#print(files[2])
+print('reshape files')
+
+#print(files[2])
+
