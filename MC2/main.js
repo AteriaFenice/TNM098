3d00542b467b09151179313bd56559d9f4847e7e
Malva <halva.malva@gmail.com>
2023-05-15 13:51:04 +0200

    slider

--- MC2/main.js
+++ MC2/main.js
@@ -19,6 +19,8 @@ roadMap.setAttribute('width', image_width)
 roadMap.setAttribute('id', 'roadMap')
 document.getElementById('map').appendChild(roadMap)
 
+
+
 const svg = d3.select("#map")
 .append("svg")
 .attr("width", image_width)
@@ -69,17 +71,173 @@ d3.json('MC2/Abila.json').then(function(json){
             (d.pageY - 10) + "px").style("left", (d.pageX + 10) + "px");
     })*/ // uncomment to check coordinates of roads
 
+    //___________________slider_____________________________________
+    // Layout for slider
+    let layout = ({
+        width: 500,
+        height: 200,
+        margin: {
+          top: 50,
+          bottom: 100,
+          left: 150,
+          right: 60
+        }
+    })
+    
+    // Slider function, example from: https://observablehq.com/@sarah37/snapping-range-slider-with-d3-brush
+    let slider = function(min, max, starting_min=min, starting_max=max) {
+        var range = [min, max + 1]
+        var starting_range = [starting_min, starting_max + 1]
+
+        // set width and height of svg
+        var w = layout.width
+        var h = layout.height
+        var margin = layout.margin
+
+        // Dimensions of slider bar
+        var width = w - margin.left - margin.right;
+        var height = h - margin.top - margin.bottom;
+
+        // create x scale
+        var x = d3.scaleLinear()
+            .domain(range) // data space
+            .range([0, width]); // display space
+
+        // create svg and translated g
+        //var svg2 = d3.select('svg')//d3.select(DOM.svg(w, h))
+        const svg2 = d3.select("#map")
+        .append("svg")
+        .attr("width", 500)
+        .attr("height", 500)
+        .attr("id", "gpsSlider")
+        const g = svg2.append('g')
+                        .attr('transform',`translate(${margin.left}, ${margin.top})`)
+
+        // draw background lines
+        g.append('g').selectAll('line')
+            .data(d3.range(range[0], range[1]+1))
+            .enter()
+            .append('line')
+            .attr('x1', d => x(d))
+            .attr('x2', d => x(d))
+            .attr('y1', 0)
+            .attr('y2', height)
+            .style('stroke', '#ccc')
+
+        // labels
+        var labelL = g.append('text')
+            .attr('id', 'labelleft')
+            .attr('x', 0)
+            .attr('y', height + 15)
+            .text(range[0])
+
+        var labelR = g.append('text')
+            .attr('id', 'labelright')
+            .attr('x', 0)
+            .attr('y', height + 15)
+            .text(range[1])
+
+        // define brush
+        var brush = d3.brushX()
+            .extent([[0, 0], [width, height]])
+            .on('brush', function(){
+                var s = d3.event.selection;
+
+                // update and move labels
+                labelL.attr('x', s[0])
+                    .text(Math.round(x.invert(s[0])))
+                labelR.attr('x', s[1])
+                    .text(Math.round(x.invert(s[1])) - 1)
+
+                // move brush handles
+                handle.attr('display', null).attr('transform', function(d, i) {
+                    return 'translate(' + [ s[i], -height / 4] + ')';
+                });
+
+                // update view
+                svg2.node().value = s.map(d => Math.round(x.invert(d)));
+                
+                // event for range sliders
+                let event = new Event('change');
+                eventhandler.dispatchEvent(event);
+            })
+            .on('end', function() {
+                if(!d3.event.sourceEvent) return;
+                var d0 = d3.event.selection.map(x.invert);
+                var d1 = d0.map(Math.round)
+                d3.select(this).transition().call(d3.event.target.move, d1.map(x))
+            })
+
+        // append brush to g
+        var gBrush = g.append('g')
+            .attr('class', 'brush')
+            .call(brush)
+
+        // add brush to handles
+        var brushResizePath = function(d) {
+            var e = +(d.type == 'e'),
+                x = e ? 1 : -1,
+                y = height / 2;
+            return 'M' + (.5 * x) + "," + y + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6) + "V" + (2 * y - 6) +
+            "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y) + "Z" + "M" + (2.5 * x) + "," + (y + 8) + "V" + (2 * y - 8) +
+            "M" + (4.5 * x) + "," + (y + 8) + "V" + (2 * y - 8);
+        }
+
+        var handle = gBrush.selectAll(".handle--custom")
+            .data([{type: "w"}, {type: "e"}])
+            .enter()
+            .append("path")
+            .attr("class", "handle--custom")
+            .attr("stroke", "#000")
+            .attr("fill", '#eee')
+            .attr("cursor", "ew-resize")
+            .attr("d", brushResizePath);
+
+        // clicking outside selected area will select small peice there
+        gBrush.selectAll('overlay')
+            .each(function(d){
+                d.type = 'selection';
+            })
+            .on('mousedown touchstart', brushcentered)
+
+        function brushcentered(){
+            var dx = x(1) - x(0),
+                cx = dd3.mouse(this)[0],
+                x0 = cx - dx / 2,
+                x1 = cx + dx / 2;
+
+            d3.select(this.parentNode).call(brush.move, x1 > width ? [width - dx, width] : x0 < 0 ? [0, dx] : [x0, x1]);
+        }
+
+        // select entire starting range
+        gBrush.call(brush.move, starting_range.map(x))
+
+        // get values
+        var getRange = function(){
+            var range = d3.brushSelection(gBrush.node()).map(d => Math.round(x.invert(d)))
+            return range
+        }
+
+        return svg2.node, {getRange : getRange} // myslider.getRange returns selected range
+
+    }
+
+   
+    //_________________________slider end____________________________
+
 
     // Load and plot gps coordinates
     d3.csv("MC2/gps.csv").then(function(data){
 
+        function updateData() {
         svg.selectAll('.pin')
         .data(data.filter(function(d){
             return d.id == '4';
         }))
         .enter()
-        .append('square', '.pin')//circle
-        .attr('r', 1)
+        .append('rect', '.pin')//circle
+        .attr('width', 1)
+        .attr('height', 1)
         .attr('fill', 'red')
         .attr('transform', function(d){
             return 'translate(' + projection([d.long, d.lat]) + ')';
@@ -108,6 +266,32 @@ d3.json('MC2/Abila.json').then(function(json){
         .attr('transform', function(d){
             return 'translate(' + projection([24.879302, 36.055626]) + ')';
         })*/
+    }
+
+    // Set range values and call slider
+    const rangeMax = Math.max.apply(null, data.Timestamp)
+    const rangeMin = Math.min.apply(null, data.Timestamp)
+
+    myslider = slider(rangeMin, rangeMax, undefined, undefined)
+    
+
+    // Update node link diagram based on slider
+    d3.select('#map')
+    .on('change', function(){
+        // change visibility of links and nodes)
+        timestamps = data.Timestamp
+        let filteredTime = timestamps
+            .filter(function (d) { 
+                timeFiltered = d.Timestamp <= myslider.getRange()[1] && d.value >= myslider.getRange()[0]; 
+                return timeFiltered
+            });
+        data = filteredTime
+        updateData()
+
+
+
+    });
+
     
     });
 
@@ -119,13 +303,15 @@ d3.json('MC2/Abila.json').then(function(json){
             return d.location == 'Kronos Mart';
         })
 
+        var rad = 10
+
         var color = d3.scaleOrdinal().domain(nodes).range(d3.schemeCategory10);
 
         var simulation = d3.forceSimulation(nodes)
         .force('charge', d3.forceManyBody().strength(5))
         .force('center', d3.forceCenter(projection([24.84842, 36.06722])[0], projection([24.84842, 36.06722])[1]))
         .force('collision', d3.forceCollide().radius(function(d){
-            return 10//d.price /10
+            return rad//d.price /10
         }))
         /*.force('cluster', d3.forceCluster().centers(function(d){
             return d.last4ccnum
@@ -133,13 +319,12 @@ d3.json('MC2/Abila.json').then(function(json){
         .on('tick', ticked)
 
         function ticked(){
-            var u = d3.select('svg')
-            .selectAll('circle')
+            svg.selectAll('circle', '.pep')
             .data(nodes)
             .join('circle')
             .attr('r', function(d){
                 //return d.price /10
-                return 10
+                return rad
             })
             .attr('fill', function(d){
                 //return 'blue'
