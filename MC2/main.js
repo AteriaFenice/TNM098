019cf207e9ff618e729fa69685139c6c4b5fe1fa
Maria <maria.brunned@gmail.com>
2023-05-17 10:40:21 +0200

    Update main.js

--- MC2/main.js
+++ MC2/main.js
@@ -44,7 +44,7 @@ let tooltip = d3.select("body")
 
 // Load GeoJson file of roads 
 d3.json('MC2/Abila.json').then(function(json){
-    //console.log(json)
+    console.log(json)
 
     var projection = d3.geoEquirectangular()
     .fitSize([image_width, image_height], json)
@@ -72,15 +72,12 @@ d3.json('MC2/Abila.json').then(function(json){
 
     // Load and plot gps coordinates
     d3.csv("MC2/gps.csv").then(function(data){
-
         svg.selectAll('.pin')
-        .data(data.filter(function(d){
-            return d.id == '4';
-        }))
+        .data(data)
         .enter()
         .append('square', '.pin')//circle
         .attr('r', 1)
-        .attr('fill', 'red')
+        .attr('color', 'red')
         .attr('transform', function(d){
             return 'translate(' + projection([d.long, d.lat]) + ')';
         })
@@ -110,67 +107,7 @@ d3.json('MC2/Abila.json').then(function(json){
         })*/
     
     });
-
-
-    // test force simulation for purchases
-    d3.csv("MC2/cc_data.csv").then(function(data){
-
-        var nodes = data.filter(function(d){
-            return d.location == 'Kronos Mart';
-        })
-
-        var color = d3.scaleOrdinal().domain(nodes).range(d3.schemeCategory10);
-
-        var simulation = d3.forceSimulation(nodes)
-        .force('charge', d3.forceManyBody().strength(5))
-        .force('center', d3.forceCenter(projection([24.84842, 36.06722])[0], projection([24.84842, 36.06722])[1]))
-        .force('collision', d3.forceCollide().radius(function(d){
-            return 10//d.price /10
-        }))
-        /*.force('cluster', d3.forceCluster().centers(function(d){
-            return d.last4ccnum
-        }).strength(0.5))*/
-        .on('tick', ticked)
-
-        function ticked(){
-            var u = d3.select('svg')
-            .selectAll('circle')
-            .data(nodes)
-            .join('circle')
-            .attr('r', function(d){
-                //return d.price /10
-                return 10
-            })
-            .attr('fill', function(d){
-                //return 'blue'
-                return color(d.last4ccnum)
-            })
-            .attr('fill-opacity', 0.5)
-            .attr('stroke', function(d){
-                //return 'blue'
-                return color(d.last4ccnum)
-            })
-            .attr('cx', function(d){
-                return d.x
-            })
-            .attr('cy', function(d){
-                return d.y
-            })
-            .on("mouseover", function() {
-                return tooltip.style("visibility", "visible");
-            })
-            .on("mousemove", function(d, i) {
-                tooltip.text('Price: ' + i.price + ' \nTime: ' + i.timestamp);
-                return tooltip.style("top",
-                    (d.pageY - 10) + "px").style("left", (d.pageX + 10) + "px");
-            })
-            .on("mouseout", function() {
-                return tooltip.style("visibility", "hidden");
-            })
-        }
-    });
 });
-
 
 
 /* Ahaggo Museum: long, lat: [24.878464, 36.07592]
